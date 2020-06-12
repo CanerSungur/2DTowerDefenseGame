757610ec37e7cefd371f5ec4c7a4d9b653ca37f3
CanerSungur <47473027+CanerSungur@users.noreply.github.com>
2020-06-12 15:30:41 +0300

    Create TowerBlueprint.cs

---
+++ TowerBlueprint.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TowerBlueprint
+{
+	public GameObject prefab;
+	public GameObject upgradedPrefab1;
+	public GameObject upgradedPrefab2;
+	public string towerName;
+	public int costToBuild;
+	public int costToUpgrade1;
+	public int costToUpgrade2;
+
+	public int sellingValue1;
+	public int sellingValue2;
+	public int sellingValue3;
+
+	public bool isUpgraded;
+	public bool isFullyUpgraded;
+
+	public int GetSellAmount()
+	{
+		return costToBuild / 2;
+	}
+	
+	public int GetUpgradeAmount()
+	{
+		return costToBuild + costToBuild * 1 / 2;
+	}
+}
