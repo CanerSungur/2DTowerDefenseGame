c7cd2e2db1e311481ebabeee592039a65be14ec5
CanerSungur <caner.sokur@outlook.com>
2020-07-01 01:19:16 +0300

    Create PlayerData.cs

---
+++ PlayerData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerData
+{
+	//Kaydedecegimiz Data
+    public int PlayerLevel { get; set; }
+	public float PlayerExperience { get; set; }
+	public float ExperienceRequired { get; set; }
+	public int AttributePoint { get; set; }
+	public int CostAttributePoint { get; set; }
+	public int FireRateAttributePoint { get; set; }
+	public int ProjectileDamageAttributePoint { get; set; }
+	public int CostModifier { get; set; }
+	public int FireRateModifier { get; set; }
+	public int ProjectileDamageModifier { get; set; }
+}
